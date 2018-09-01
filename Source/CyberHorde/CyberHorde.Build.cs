41da4042b31fdab2923ffb6f1b7fed096b080a9e
Tom Delaney <delaneyt1@gmail.com>
2018-09-01 15:32:22 -0400

    Initial commit

---
+++ Source/CyberHorde/CyberHorde.Build.cs
@@ -0,0 +1,13 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class CyberHorde : ModuleRules
+{
+	public CyberHorde(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "OnlineSubsystem", "OnlineSubsystemSteam" });
+	}
+}
